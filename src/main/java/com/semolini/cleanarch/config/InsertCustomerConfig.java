e9e9952c702369d564d50e65c524b461a1be1343
Jean Semolini <jean.maiko@hotmail.com>
2022-12-30 15:38:15 -0300

    feat: criando o producer

--- src/main/java/com/semolini/cleanarch/config/InsertCustomerConfig.java
+++ src/main/java/com/semolini/cleanarch/config/InsertCustomerConfig.java
@@ -3,6 +3,7 @@ package com.semolini.cleanarch.config;
 import com.semolini.cleanarch.core.usecase.impl.InsertCustomerUseCaseImpl;
 import com.semolini.cleanarch.dataprovider.FindAddressByZipCodeImpl;
 import com.semolini.cleanarch.dataprovider.InsertCustomerImpl;
+import com.semolini.cleanarch.dataprovider.SendCpfForValidationImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +13,9 @@ public class InsertCustomerConfig {
     @Bean
     public InsertCustomerUseCaseImpl insertCustomerUseCase(
             FindAddressByZipCodeImpl findAddressByZipCode,
-            InsertCustomerImpl insertCustomer
+            InsertCustomerImpl insertCustomer,
+            SendCpfForValidationImpl sendCpfForValidation
     ) {
-        return new InsertCustomerUseCaseImpl(findAddressByZipCode, insertCustomer);
+        return new InsertCustomerUseCaseImpl(findAddressByZipCode, insertCustomer, sendCpfForValidation);
     }
 }
---
+++ src/main/java/com/semolini/cleanarch/core/dataprovider/SendCpfForValidation.java
@@ -0,0 +1,6 @@
+package com.semolini.cleanarch.core.dataprovider;
+
+public interface SendCpfForValidation {
+
+    void send(final String cpf);
+}
